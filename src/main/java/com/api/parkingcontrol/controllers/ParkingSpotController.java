2ec25cb3e78ecea468c92fbf6f50f9bba9cf4d90
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-02 07:07:46 -0300

    Criação do método update

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,6 +79,22 @@ public class ParkingSpotController {
         }
         parkingSpotService.deleteParkingSpot(parkingSpotModelOptional.get().getId());
         return ResponseEntity.status(HttpStatus.OK).body("Parking Spot Deleted successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id, @RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        ParkingSpotModel parkingSpotModelForUpdated = parkingSpotService.getOneParkingSpot(id).get();
+
+        parkingSpotModelForUpdated.setApartment(parkingSpotDto.getApartment());
+        parkingSpotModelForUpdated.setBlock(parkingSpotDto.getBlock());
+        parkingSpotModelForUpdated.setBrandCar(parkingSpotDto.getBrandCar());
+        parkingSpotModelForUpdated.setColorCar(parkingSpotDto.getColorCar());
+        parkingSpotModelForUpdated.setLicensePlateCar(parkingSpotDto.getLicensePlateCar());
+        parkingSpotModelForUpdated.setModelCar(parkingSpotDto.getModelCar());
+        parkingSpotModelForUpdated.setParkingSpotNumber(parkingSpotDto.getParkingSpotNumber());
+        parkingSpotModelForUpdated.setResponsibleName(parkingSpotDto.getResponsibleName());
+
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModelForUpdated));
     }
 
     
