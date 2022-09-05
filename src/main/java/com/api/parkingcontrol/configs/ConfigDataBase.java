a341dab63032f97093208547ef02ba824d396948
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-05 06:56:14 -0300

    Adicionando informações em memória para o banco

---
+++ src/main/java/com/api/parkingcontrol/configs/ConfigDataBase.java
@@ -0,0 +1,45 @@
+package com.api.parkingcontrol.configs;
+
+import org.springframework.boot.CommandLineRunner;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepostitory;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ConfigDataBase{
+
+    private final ParkingSpotRepostitory parkingSpotRepostitory;
+
+    private CommandLineRunner execute() {
+        return args -> {
+            ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
+            parkingSpotModel.setParkingSpotNumber("205");
+            parkingSpotModel.setApartment("202");
+            parkingSpotModel.setBlock("9");
+            parkingSpotModel.setBrandCar("HONDA");
+            parkingSpotModel.setColorCar("WHITE");
+            parkingSpotModel.setModelCar("HRV");
+            parkingSpotModel.setLicensePlateCar("RRS856");
+            parkingSpotModel.setResponsibleName("Carlos Daniel");
+
+            ParkingSpotModel newParkingSpotModel = new ParkingSpotModel();
+            newParkingSpotModel.setParkingSpotNumber("232H");
+            newParkingSpotModel.setApartment("45");
+            newParkingSpotModel.setBlock("A");
+            newParkingSpotModel.setBrandCar("Chevrolet");
+            newParkingSpotModel.setColorCar("gray");
+            newParkingSpotModel.setModelCar("TRAKER");
+            newParkingSpotModel.setLicensePlateCar("RFG5454");
+            newParkingSpotModel.setResponsibleName("Maicon Cardoso");
+
+            parkingSpotRepostitory.save(parkingSpotModel);
+            parkingSpotRepostitory.save(newParkingSpotModel);
+        };
+    }
+
+    
+    
+    
+}
