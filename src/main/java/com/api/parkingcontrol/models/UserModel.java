a17a22a011c3c391fae9ff404ae3030c861dea92
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-08 11:55:54 -0300

    Criação de entidade para usuário

---
+++ src/main/java/com/api/parkingcontrol/models/UserModel.java
@@ -0,0 +1,60 @@
+package com.api.parkingcontrol.models;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "TB_USER")
+@Data
+public class UserModel implements UserDetails, Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column(nullable = false, length = 80, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
