6790f4f86a3cfb8c32f265043b4f1af1623836ba
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-08 11:55:27 -0300

    Criação de classe de serviço para inserção de usuário

---
+++ src/main/java/com/api/parkingcontrol/configs/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.api.parkingcontrol.configs;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.models.UserModel;
+import com.api.parkingcontrol.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with username" + username));
+        return userModel;
+    }
+    
+}
