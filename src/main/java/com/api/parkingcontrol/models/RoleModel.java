041f9504ae05c5ea44cc00e6daeecea9f9778c63
maicon_cardoso <maiconscardoso@hotmail.com>
2022-09-09 06:46:55 -0300

    Criação da classe RoleModel

---
+++ src/main/java/com/api/parkingcontrol/models/RoleModel.java
@@ -0,0 +1,39 @@
+package com.api.parkingcontrol.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import com.api.parkingcontrol.enums.RoleName;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "TB_ROLE")
+@Data
+public class RoleModel implements Serializable, GrantedAuthority{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName roleName;
+
+    @Override
+    public String getAuthority() {
+        return this.roleName.toString();
+    }
+    
+}
